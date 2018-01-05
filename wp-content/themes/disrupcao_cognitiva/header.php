06a17b1880f265bc995defbc0f283fc3332cdcd1
Jehny <jehnyffen@gmail.com>
2018-01-04 22:27:56 -0300

    primeiro commit

---
+++ wp-content/themes/disrupcao_cognitiva/header.php
@@ -0,0 +1,14 @@
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/bootstrap.css" type="text/css" />
+		<link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/dc.css" type="text/css" />
+		<link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/medias.css" type="text/css" />
+		
+		<script src="<?php bloginfo('template_url'); ?>/js/jquery-3.0.0.min.js"></script>
+		<script src="<?php bloginfo('template_url'); ?>/js/bootstrap.js"></script>
+
+		<title>Disrupção Cognitiva</title>
+
+</html>
